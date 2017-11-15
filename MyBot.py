900ad31364d334e8bfd0bcf611e9e54ece830112
Jared Newell <jared.newell@gmail.com>
2017-11-15 00:30:54 -0800

    added a timeout at 1.8s to stop me from timeing out

--- MyBot.py
+++ MyBot.py
@@ -14,10 +14,11 @@ to log anything use the logging module.
 import hlt
 # Then let's import the logging module so we can print out information
 import logging
+from datetime import datetime, timedelta
 
 # GAME START
 # Here we define the bot's name as Settler and initialize the game, including communication with the Halite engine.
-game = hlt.Game("Settler")
+game = hlt.Game("Jbot")
 # Then we print our start message to the logs
 LOG = logging.getLogger('jbot')
 LOG.setLevel(logging.INFO)
@@ -48,8 +49,13 @@ def get_nearest_unowned_planet_for_ship(game_map, ship):
                 if not entity.is_owned():
                     return entity
 
+timedelta_2s = timedelta(seconds=2)
+timedelta_buffer = timedelta(milliseconds=200)
 while True:
     # TURN START
+    time_turnstart = datetime.utcnow()
+    time_timeout = time_turnstart + timedelta_2s
+
     # Update the map for the new turn and get the latest version
     game_map = game.update_map()
 
@@ -58,6 +64,13 @@ while True:
 
     # For every ship that I control
     for ship in game_map.get_me().all_ships():
+        time_now = datetime.utcnow()
+
+        # End turn early if needed due to timeout.
+        if time_now + timedelta_buffer >= time_timeout:
+            LOG.critical('BAILING TURN FOR TIMEOUT')
+            break  # break out of the per ship loop, which should send our commands
+
         nearest_planet = get_nearest_planet_for_ship(game_map, ship)
         nearest_unowned_planet = get_nearest_unowned_planet_for_ship(game_map, ship)
         LOG.debug('ship %s, nearest ANY_planet: %s', ship, nearest_planet)
